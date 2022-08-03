23858a22219266d458eafa297ca48d1a8b081e4e
Matheus de Mello Bueno <matheus.mello@cds-software.com.br>
2022-08-03 16:21:35 -0300

    atualizando layout e adicionando componente profiler - inicio

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -10,11 +11,6 @@ const IndexPage = () => (
   </Layout>
 )
 
-/**
- * Head export to define metadata for the page
- *
- * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
- */
 export const Head = () => <Seo title="Home" />
 
 export default IndexPage
