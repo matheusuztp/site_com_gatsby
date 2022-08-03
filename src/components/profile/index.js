c18f49d45bafbc5df121c24a3766830b8884aa74
Matheus de Mello Bueno <matheus.mello@cds-software.com.br>
2022-08-03 16:17:15 -0300

    atualizando layout e adicionando componente profiler - inicio

---
+++ src/components/profile/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Profile = () => (
+    <div className="Profile-wrapper">
+        <h1>Malucao dos codigos</h1>
+        <h2>Faz programa</h2>
+        <p>
+            Membro de uma equipe de codigos e programas que fazem coisas
+        </p>
+    </div>
+)
+
+export default Profile
