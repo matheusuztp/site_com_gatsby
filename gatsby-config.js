a47826226eed8702f7e9987fcfec434bff5b44d8
Matheus de Mello Bueno <matheus.mello@cds-software.com.br>
2022-08-03 15:37:14 -0300

    primeiro commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Sitezinho bom`,
+    description: `Testando a nova modalidade de codigos magnificos que se chama Gatsby`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
